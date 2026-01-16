61fcb3121ece8b5b73a71af453bdbe916b162ba6
barbs060 <barbararm6@gmail.com>
2026-01-16 16:07:25 -0700

    Actualizacion del proyecto (cambios de archivos)

---
+++ Pages/js/MV.js
@@ -0,0 +1,9 @@
+function mostrarVideo1() {
+    document.getElementById("imagen-container-1").style.display = "none";
+    document.getElementById("video-1").style.display = "block";
+}
+
+function mostrarVideo2() {
+    document.getElementById("imagen-container-2").style.display = "none";
+    document.getElementById("video-2").style.display = "block";
+}
